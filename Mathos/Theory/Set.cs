6d4a3629afd8869a6b2207103b8d9720d07b145c
napen123 <napen123@gmail.com>
2015-05-06 18:28:12 -0600

    Added set theory.

---
+++ Mathos/Theory/Set.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mathos.Theory
+{
+    /// <summary>
+    /// An implementation of sets from set theory.
+    /// 
+    /// - Needs cartesian product.
+    /// - Needs power set.
+    /// </summary>
+    public class Set<T> : IEnumerable
+    {
+        /// <summary>
+        /// The contents of the set.
+        /// </summary>
+        public readonly HashSet<T> Elements;
+
+        /// <summary>
+        /// Create an empty set.
+        /// </summary>
+        public Set()
+        {
+            Elements = new HashSet<T>();
+        }
+
+        /// <summary>
+        /// Create a set from an already existing set.
+        /// </summary>
+        /// <param name="s">The base set to copy from.</param>
+        public Set(Set<T> s)
+        {
+            Elements = s.Elements;
+        }
+
+        /// <summary>
+        /// Create a set from a list of elements.
+        /// </summary>
+        /// <param name="elements">The elements the set will contain.</param>
+        public Set(params T[] elements) : this()
+        {
+            foreach(var element in elements)
+                Elements.Add(element);
+        }
+
+        
+        /// <summary>
+        /// Add an element to the set.
+        /// </summary>
+        /// <param name="element">The element to add.</param>
+        /// <returns>This set with the given element added.</returns>
+        public HashSet<T> Add(T element)
+        {
+            Elements.Add(element);
+
+            return Elements;
+        }
+
+        /// <summary>
+        /// Add multiple elements to the set.
+        /// </summary>
+        /// <param name="elements">The elements to add.</param>
+        /// /// <returns>This set with the given elements added.</returns>
+        public Set<T> Add(params T[] elements)
+        {
+            foreach(var element in elements)
+                Elements.Add(element);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Add the contents of another set into this one.
+        /// </summary>
+        /// <param name="b">The base set.</param>
+        /// <returns>This set alongside set b combined.</returns>
+        public Set<T> Add(Set<T> b)
+        {
+            foreach(var element in b.Elements)
+                Elements.Add(element);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Remove the given item from the set.
+        /// </summary>
+        /// <param name="item">The item to remove.</param>
+        /// <returns>This set with the given item removed.</returns>
+        public Set<T> Remove(T item)
+        {
+            Elements.Remove(item);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Remove the item that matches the given expression.
+        /// </summary>
+        /// <param name="match">The expression to match to.</param>
+        /// <returns>This set with the item at "i" removed.</returns>
+        public Set<T> RemoveMatch(Predicate<T> match)
+        {
+            Elements.RemoveWhere(match);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Get the union of this set and b.
+        /// </summary>
+        /// <param name="b">The second set.</param>
+        /// <returns>this ∪ b.</returns>
+        public Set<T> Union(Set<T> b)
+        {
+            var ret = new Set<T>();
+
+            foreach (var element in Elements.Concat(b.Elements).Where(item => !ret.Elements.Contains(item)))
+                ret.Add(element);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Get the intersection of this set and b.
+        /// </summary>
+        /// <param name="b">The second set.</param>
+        /// <returns>this ∩ b.</returns>
+        public Set<T> Intersection(Set<T> b)
+        {
+            var ret = new Set<T>();
+
+            foreach (var element in Elements.Where(element => b.Elements.Contains(element)))
+                ret.Add(element);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Get the difference of this set and b.
+        /// </summary>
+        /// <param name="b">The second set.</param>
+        /// <returns>this \ b.</returns>
+        public Set<T> Difference(Set<T> b)
+        {
+            var ret = new Set<T>();
+
+            foreach (var element in Elements.Where(element => !b.Elements.Contains(element)))
+                ret.Add(element);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Get the symmetric difference of this set and b.
+        /// </summary>
+        /// <param name="b">The second set.</param>
+        /// <returns>Either (this △ b) or (this ⊖ b).</returns>
+        public Set<T> Symmetric(Set<T> b)
+        {
+            var ret = new Set<T>();
+            var union = Union(b);
+            var inter = Intersection(b);
+
+            return union.Difference(inter);
+        }    
+
+        /// <summary>
+        /// Is this set a subset of b?
+        /// </summary>
+        /// <param name="b">The base set.</param>
+        /// <returns>this ⊆ b.</returns>
+        public bool IsSubset(Set<T> b)
+        {
+            return Elements.IsSubsetOf(b.Elements);
+        }
+
+        /// <summary>
+        /// Is this set a proper subset of b?
+        /// </summary>
+        /// <param name="b">The base set.</param>
+        /// <returns>this ⊂ b.</returns>
+        public bool IsProperSubset(Set<T> b)
+        {
+            return Elements.IsProperSubsetOf(b.Elements);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// The enumerator of the set.
+        /// </summary>
+        /// <returns>The contents of the set.</returns>
+        public IEnumerator GetEnumerator()
+        {
+            return Elements.GetEnumerator();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Set<T>))
+                return false;
+
+            return ((Set<T>) obj).Elements.All(arg => Elements.Contains(arg));
+        }
+
+        public override int GetHashCode()
+        {
+            return (Elements != null ? Elements.GetHashCode() : 0);
+        }
+
+        public override string ToString()
+        {
+            var str = Elements.Aggregate("{", (current, element) => current + (element.ToString() + ","));
+
+            if (str.EndsWith(","))
+                str = str.Substring(0, str.Length - 1);
+
+            return str + "}";
+        }
+    }
+}
---
+++ MathosTest/Mathos Main/Theory/SetTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Mathos.Theory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathosTest.Mathos_Main.Theory
+{
+    [TestClass]
+    public class SetTest
+    {
+        [TestMethod]
+        public void AddRemoveElements()
+        {
+            var set = new Set<int>(2, 4, 6, 8, 10);
+
+            Console.WriteLine(set.ToString());
+
+            set.Add(1, 3, 5, 7, 9);
+
+            Console.WriteLine(set.ToString());
+
+            set.Remove(9);
+
+            Console.WriteLine(set.ToString());
+        }
+
+        [TestMethod]
+        public void AddSets()
+        {
+            var a = new Set<int>(1, 2, 3, 4, 5);
+            var b = new Set<int>(6, 7, 8, 9, 10);
+
+            Console.WriteLine(a.Add(b).ToString());
+        }
+
+        [TestMethod]
+        public void UnionTest()
+        {
+            var a = new Set<int>(1, 2, 3);
+            var b = new Set<int>(2, 3, 4);
+
+            Console.WriteLine(a.Union(b));
+        }
+
+        [TestMethod]
+        public void IntersectionTest()
+        {
+            var a = new Set<int>(1, 2, 3);
+            var b = new Set<int>(2, 3, 4);
+
+            Console.WriteLine(a.Intersection(b));
+        }
+
+        [TestMethod]
+        public void DifferenceTest()
+        {
+            var a = new Set<int>(1, 2, 3);
+            var b = new Set<int>(2, 3, 4);
+
+            Console.WriteLine(a.Difference(b));
+            Console.WriteLine(b.Difference(a));
+        }
+
+        [TestMethod]
+        public void SymmetricTest()
+        {
+            var a = new Set<int>(1, 2, 3);
+            var b = new Set<int>(2, 3, 4);
+
+            Console.WriteLine(a.Symmetric(b));
+        }
+
+        [TestMethod]
+        public void SubsetTest()
+        {
+            var a = new Set<int>(1, 2, 3);
+            var b = new Set<int>(1, 2, 3);
+            var c = new Set<int>(1, 2, 3, 4);
+
+            Console.WriteLine(a + @" ⊆ " + b + @" = " + a.IsSubset(b));
+            Console.WriteLine(a + @" ⊆ " + c + @" = " + a.IsSubset(c));
+
+            Console.WriteLine(a + @" ⊂ " + b + @" = " + a.IsProperSubset(b));
+            Console.WriteLine(a + @" ⊂ " + c + @" = " + a.IsProperSubset(c));
+        }
+
+        [TestMethod]
+        public void TechnicalTesting()
+        {
+            var set = new Set<int>(1, 2, 3, 4);
+
+            Console.WriteLine(set.ToString());
+            Console.WriteLine(set.GetHashCode());
+            Console.WriteLine(set.Equals(new Set<int>(1, 2, 3, 4)));
+        }
+    }
+}
