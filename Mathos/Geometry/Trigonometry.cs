e7376ed2f6e2ac700771d5e9ea19db32ec0842f8
napen123 <napen123@gmail.com>
2015-12-05 16:16:11 -0700

    Add some simple trigonometry methods.

--- Mathos/Geometry/Trigonometry.cs
+++ Mathos/Geometry/Trigonometry.cs
@@ -1,9 +1,83 @@
-﻿// TODO: Needs implementation
-/*namespace Mathos.Geometry.Trigonometry
+﻿using Mathos.Geometry.Shapes;
+
+namespace Mathos.Geometry
 {
-    public static class Get
+    /// <summary>
+    ///       A
+    ///       |\
+    /// SideA | \ SideC
+    ///       |  \
+    ///      B----- C
+    ///       SideB
+    /// 
+    /// Note: Right angles should be used.
+    /// </summary>
+    public static class Trigonometry
     {
-        public static double Area 
+        /// <summary>
+        /// Angles of a triangle.
+        /// </summary>
+        public enum TriangleAngle
+        {
+            /// <summary>
+            /// Angle A (Top)
+            /// </summary>
+            A,
+            /// <summary>
+            /// Angle C (Corner)
+            /// </summary>
+            C
+        }
+
+        /// <summary>
+        /// Calculates the sine of <paramref name="angle"/> in <paramref name="tri"/>.
+        /// </summary>
+        /// <param name="tri">The triangle</param>
+        /// <param name="angle">The angle to calculate.</param>
+        /// <returns>The sine of <paramref name="angle"/>.</returns>
+        public static double Sine(Triangle tri, TriangleAngle angle = TriangleAngle.C)
+        {
+            switch (angle)
+            {
+                case TriangleAngle.A:
+                    return tri.SideB/tri.SideC;
+                default:
+                    return tri.AngleA/tri.SideC;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the cosine of <paramref name="angle"/> in <paramref name="tri"/>.
+        /// </summary>
+        /// <param name="tri">The triangle</param>
+        /// <param name="angle">The angle to calculate.</param>
+        /// <returns>The cosine of <paramref name="angle"/>.</returns>
+        public static double Cosine(Triangle tri, TriangleAngle angle = TriangleAngle.C)
+        {
+            switch (angle)
+            {
+                case TriangleAngle.A:
+                    return tri.SideA / tri.SideC;
+                default:
+                    return tri.AngleB / tri.SideC;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the tangent of <paramref name="angle"/> in <paramref name="tri"/>.
+        /// </summary>
+        /// <param name="tri">The triangle</param>
+        /// <param name="angle">The angle to calculate.</param>
+        /// <returns>The tangent of <paramref name="angle"/>.</returns>
+        public static double Tangent(Triangle tri, TriangleAngle angle = TriangleAngle.C)
+        {
+            switch (angle)
+            {
+                case TriangleAngle.A:
+                    return tri.SideB / tri.SideA;
+                default:
+                    return tri.AngleA / tri.SideB;
+            }
+        }
     }
 }
-*/
---
+++ MathosTest/Geometry/Trigonometry.cs
@@ -0,0 +1,15 @@
+﻿using Mathos.Geometry.Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathosTest.Geometry
+{
+    [TestClass]
+    public class Trigonometry
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var triangle = new Triangle();
+        }
+    }
+}
