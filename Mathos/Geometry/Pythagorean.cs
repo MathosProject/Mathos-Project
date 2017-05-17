7d92fca0d949e429c9dbe63a9c8936df02642c10
Ethan Dagner <napen123@gmail.com>
2017-05-16 22:14:07 -0600

    Update Pythagorean

--- Mathos/Geometry/Pythagorean.cs
+++ Mathos/Geometry/Pythagorean.cs
@@ -3,34 +3,38 @@
 namespace Mathos.Geometry
 {
     /// <summary>
-    /// Static class used to find the length of a side of a right triangle
+    /// Used to find the length of a side of a right triangle.
     /// </summary>
     public static class Pythagorean
     {
         /// <summary>
-        /// Static method that returns the hypotenuse given two sides of a right triangle
+        /// Get the hypotenuse given two sides of a right triangle.
         /// </summary>
-        /// <param name="side1">Length of the first side</param>
-        /// <param name="side2">Length of the second side</param>
-        /// <returns>Length of the hypotenuse</returns>
+        /// <param name="side1">Length of the first side.</param>
+        /// <param name="side2">Length of the second side.</param>
+        /// <returns>Length of the hypotenuse.</returns>
         public static double FindHypotenuse(double side1, double side2)
         {
-            if (side1 <= 0 || side2 <= 0)
-                throw new ArgumentOutOfRangeException("Invalid length for sides");
+            if (side1 <= 0)
+                throw new ArgumentOutOfRangeException(nameof(side1));
+            if (side2 <= 0)
+                throw new ArgumentOutOfRangeException(nameof(side2));
 
             return Math.Sqrt(side1 * side1 + side2 * side2);
         }
 
         /// <summary>
-        /// Static method that returns the length of a side given the side and hypotenuse of a right triangle
+        /// Get the length of a side given another side and the hypotenuse of a right triangle.
         /// </summary>
-        /// <param name="side1">Length of the first side</param>
-        /// <param name="hypotenuse">Length of the hypotenuse</param>
+        /// <param name="side1">Length of a side.</param>
+        /// <param name="hypotenuse">Length of the hypotenuse.</param>
         /// <returns>Length of the remaining side</returns>
-        public static double FindNonHypotenuse(double side1, double hypotenuse)
+        public static double SideFromHypotenuse(double side1, double hypotenuse)
         {
-            if (side1 <= 0 || hypotenuse <= 0 || side1 >= hypotenuse)
-                throw new ArgumentOutOfRangeException("Invalid length for side or hypotenuse");
+            if (side1 <= 0 || side1 >= hypotenuse)
+                throw new ArgumentOutOfRangeException(nameof(side1));
+            if (hypotenuse <= 0)
+                throw new ArgumentOutOfRangeException(nameof(hypotenuse));
 
             return Math.Sqrt(hypotenuse * hypotenuse - side1 * side1);
         }
