b1cc055dcde2084eeb5e7ac7057860b7baf5dfb8
Ethan Dagner <napen123@gmail.com>
2017-07-25 16:08:19 -0600

    Update IntegerArithmetic xml-comments

--- Mathos/Arithmetic/IntegerArithmetic.cs
+++ Mathos/Arithmetic/IntegerArithmetic.cs
@@ -3,15 +3,15 @@
 namespace Mathos.Arithmetic
 {
     /// <summary>
-    /// 
+    /// Provides methods for performing common arithmetic operations on <see cref="int"/>.
     /// </summary>
     public static class IntegerArithmetic
     {
         /// <summary>
-        /// 
+        /// Get the square root of <paramref name="number"/>.
         /// </summary>
-        /// <param name="number"></param>
-        /// <returns></returns>
+        /// <param name="number">The number to get the square root of.</param>
+        /// <returns>The square root of <paramref name="number"/>.</returns>
         public static int Sqrt(int number)
         {
             var res = 0;
@@ -37,21 +37,21 @@ namespace Mathos.Arithmetic
         }
 
         /// <summary>
-        /// 
+        /// Get the square of <paramref name="x"/>.
         /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
+        /// <param name="x">The number to square.</param>
+        /// <returns>The square of <paramref name="x"/>.</returns>
         public static long Sqr(this int x)
         {
             return x <= Constants.MaxIntSqrt ? x*x : Math.BigMul(x, x);
         }
 
         /// <summary>
-        /// 
+        /// Get the square of <paramref name="x"/>.
         /// </summary>
-        /// <param name="x"></param>
-        /// <returns></returns>
-        /// <exception cref="OverflowException"></exception>
+        /// <param name="x">The number to square.</param>
+        /// <returns>The square of <paramref name="x"/>.</returns>
+        /// <exception cref="OverflowException"><paramref name="x"/> is greater than <see cref="Constants.MaxLongSqrt"/>.</exception>
         public static long Sqr(this long x)
         {
             if (x <= Constants.MaxLongSqrt)
@@ -61,11 +61,11 @@ namespace Mathos.Arithmetic
         }
 
         /// <summary>
-        /// 
+        /// Raise <paramref name="x"/> to the power of <paramref name="n"/>.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <returns></returns>
+        /// <param name="x">The number to raise.</param>
+        /// <param name="n">The power to raise to.</param>
+        /// <returns><paramref name="x"/> to the power of <paramref name="n"/>.</returns>
         public static int Pow(this int x, uint n)
         {
             var result = 1;
@@ -85,21 +85,22 @@ namespace Mathos.Arithmetic
         }
 
         /// <summary>
+        /// Raise <paramref name="x"/> to the power of <paramref name="n"/>.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <returns></returns>
+        /// <param name="x">The number to raise.</param>
+        /// <param name="n">The power to raise to.</param>
+        /// <returns><paramref name="x"/> to the power of <paramref name="n"/>.</returns>
         public static double Pow(this int x, int n)
         {
             return n < 0 ? Convert.ToDouble(x).Pow(n) : x.Pow(Convert.ToUInt32(n));
         }
 
         /// <summary>
-        /// 
+        /// Raise <paramref name="x"/> to the power of <paramref name="n"/>.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <returns></returns>
+        /// <param name="x">The number to raise.</param>
+        /// <param name="n">The power to raise to.</param>
+        /// <returns><paramref name="x"/> to the power of <paramref name="n"/>.</returns>
         public static long Pow(this long x, uint n)
         {
             long result = 1;
@@ -119,10 +120,11 @@ namespace Mathos.Arithmetic
         }
 
         /// <summary>
+        /// Raise <paramref name="x"/> to the power of <paramref name="n"/>.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="n"></param>
-        /// <returns></returns>
+        /// <param name="x">The number to raise.</param>
+        /// <param name="n">The power to raise to.</param>
+        /// <returns><paramref name="x"/> to the power of <paramref name="n"/>.</returns>
         public static double Pow(this long x, int n)
         {
             return n < 0 ? Convert.ToDouble(x).Pow(n) : x.Pow(Convert.ToUInt32(n));
